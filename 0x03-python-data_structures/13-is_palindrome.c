b664d224d0eea7f33840ff75b4207433c150a335
Jennifer <Iruomaonyia@outlook.com>
2022-09-27 13:24:50 +0100

    Everything

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,10 +8,10 @@ int *get_end(int, listint_t *);
  */
 int is_palindrome(listint_t **head)
 {
-	int length = 0, limit, i, iter = 1, *check;
+	int length = 0, limit, i, j, iter = 1, *check;
 	listint_t *cnter = *head;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	while (cnter != NULL)
 	{
@@ -24,16 +24,19 @@ int is_palindrome(listint_t **head)
 		limit = (length / 2)  + 1;
 	check = get_end(limit, *head);
 	cnter = *head;
+	j = limit;
 	while (iter < limit)
 	{
 		i = cnter->n;
-		if (i != check[limit - 1])
+		if (i != check[j - 1])
 		{
+			/*printf("%d - i, %d - (j-1), %d - (limit - 1)\n", i, j -1, limit - 1);*/
 			free(check);
 			return (0);
 		}
 		iter++;
 		length--;
+		j--;
 		if (iter != limit)
 			cnter = cnter->next;
 	}
@@ -52,12 +55,23 @@ int *get_end(int limit, listint_t *h)
 	int i = 0, *endpoints;
 	listint_t *curr = h;
 
-	endpoints = malloc(sizeof(int) * limit);
+	endpoints = malloc(sizeof(int) * (limit + 1));
+	while (i < limit)
+	{
+		curr = curr->next;
+		i++;
+	}
+	i = 0;
+	if (curr != NULL)
+	{
 	while (i < limit)
 	{
 		endpoints[i] = curr->n;
 		curr = curr->next;
 		i++;
 	}
+	}
+	else
+		endpoints[i] = curr->n;
 	return (endpoints);
 }
