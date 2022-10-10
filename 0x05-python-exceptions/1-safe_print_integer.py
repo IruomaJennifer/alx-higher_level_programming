1cc7403a7ae1c20148251b41d3afeba2f85433f5
Jennifer <Iruomaonyia@outlook.com>
2022-10-10 20:44:55 +0100

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        i = int(value)
+        print("{:d}".format(i))
+        return True
+    except ValueError:
+        return False
